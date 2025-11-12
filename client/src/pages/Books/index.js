f3b8cadff2e0d0d895c0cb76fa59f16d253c292c
Kaio Garcia - Desenvolvimento - Employer <kaiogarcia@employer.com.br>
2025-11-12 11:52:58 -0300

    client-react-js

---
+++ client/src/pages/Books/index.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../../services/api";
+import "./styles.css";
+import logoImage from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { FiPower, FiEdit, FiTrash2 } from "react-icons/fi";
+
+export default function Books() {
+  const [books, setBooks] = useState([]);
+  const userName = localStorage.getItem("userName");
+  const accessToken = localStorage.getItem("accessToken");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    api
+      .get("api/Book/v1/asc/20/1", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((reponse) => {
+        setBooks(reponse.data.list);
+      });
+  }, [accessToken]);
+
+  async function logout() {
+    try {
+      await api.get("/api/auth/v1/revoke", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      localStorage.clear();
+      navigate("/");
+    } catch (error) {
+      alert("Logout failed! Try again!");
+    }
+  }
+
+  async function deleteBook(id) {
+    try {
+      await api.delete(`api/Book/v1/${id}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      setBooks(books.filter((book) => book.id !== id));
+    } catch (error) {
+      alert("Delete failed! Try again!");
+    }
+  }
+
+  return (
+    <div className="book-container">
+      <header>
+        <img src={logoImage} alt="Logo" />
+        <span>
+          Welcome, <strong>{userName.toUpperCase()}</strong>
+        </span>
+        <Link className="button" to="/book/new">
+          Add New book
+        </Link>
+        <button onClick={logout} type="button">
+          <FiPower size={19} color="#251fc5" />
+        </button>
+      </header>
+
+      <h1>Registered Books</h1>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            <strong>Title:</strong>
+            <p>{book.title}</p>
+            <strong>Author:</strong>
+            <p>{book.author}</p>
+            <strong>Price:</strong>
+            <p>
+              {Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              }).format(book.price)}
+            </p>
+            <strong>Release Date:</strong>
+            <p>
+              {Intl.DateTimeFormat("pt-BR").format(new Date(book.launchDate))}
+            </p>
+
+            <button type="button">
+              <FiEdit size={20} color="#251fc5" />
+            </button>
+
+            <button onClick={() => deleteBook(book.id)} type="button">
+              <FiTrash2 size={20} color="#251fc5" />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ client/src/pages/NewBook/index.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import "./styles.css";
+import logoImage from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
+import api from "../../services/api";
+import { useNavigate } from "react-router-dom";
+
+export default function Newbook() {
+  const [author, setAuthor] = useState("");
+  const [title, setTitle] = useState("");
+  const [launchDate, setLaunchDate] = useState("");
+  const [price, setPrice] = useState("");
+
+  const navigate = useNavigate();
+
+  async function createNewBook(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      author,
+      launchDate,
+      price,
+    };
+
+    const accessToken = localStorage.getItem("accessToken");
+
+    try {
+      await api.post("api/Book/v1", data, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+    } catch (error) {
+      alert("Error while recording Book! Try again!");
+    }
+    navigate("/books");
+  }
+
+  return (
+    <div className="new-book-container">
+      <div className="content">
+        <section className="form">
+          <img src={logoImage} alt="Logo" />
+          <h1>Add New Book</h1>
+          <p>Enter the book information and click on 'Add'!</p>
+          <Link className="blac-link" to="/books">
+            <FiArrowLeft size={16} color="#251fc5" />
+            Home
+          </Link>
+        </section>
+        <form onSubmit={createNewBook}>
+          <input
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <input
+            placeholder="Author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+          <input
+            type="date"
+            value={launchDate}
+            onChange={(e) => setLaunchDate(e.target.value)}
+          />
+          <input
+            placeholder="Price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <button className="button" type="submit">
+            Add
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
